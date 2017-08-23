f0068658404c6c65bca57e6e491683c1df7b68c4
arthers3king <31269678+arthers3king@users.noreply.github.com>
2017-08-23 15:02:19 +0700

    Update index.php

--- index.php
+++ index.php
@@ -20,7 +20,7 @@ if($arrJson['events'][0]['message']['text'] == "สวัสดี"){
   $arrPostData = array();
   $arrPostData['replyToken'] = $arrJson['events'][0]['replyToken'];
   $arrPostData['messages'][0]['type'] = "text";
-  $arrPostData['messages'][0]['text'] = "ฉันเป็นระบบอัตโนมัติของฝ่ายทะเบียน";
+  $arrPostData['messages'][0]['text'] = "ฉันชื่อกิมลี";
 }else if($arrJson['events'][0]['message']['text'] == "ทำอะไรได้บ้าง"){
   $arrPostData = array();
   $arrPostData['replyToken'] = $arrJson['events'][0]['replyToken'];
