d4c68b5f4b7030f2ab09b5e07026c05cad553a29
arthers3king <31269678+arthers3king@users.noreply.github.com>
2017-08-23 15:49:51 +0700

    Update index.php

--- index.php
+++ index.php
@@ -10,9 +10,9 @@ if (!is_null($events['events'])) {
 	// Loop through each event
 	foreach ($events['events'] as $event) {
 		// Reply only when message sent is in 'text' format
-		if ($event['type'] == 'สวัสดี' && $event['message']['type'] == 'text') {
+		if ($event['type'] == 'message' && $event['message']['type'] == 'text') {
 			// Get text sent
-			$text = $event['message']['text'];
+			$text = $event['message']['text']== สวัสดี;
 			// Get replyToken
 			$replyToken = $event['replyToken'];
 
