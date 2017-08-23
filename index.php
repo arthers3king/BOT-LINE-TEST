89ec1a1403b1be1c729ba40ac72c1aa075a0db7b
arthers3king <31269678+arthers3king@users.noreply.github.com>
2017-08-23 14:08:58 +0700

    Update index.php

--- index.php
+++ index.php
@@ -1,2 +1,18 @@
-<?php
-echo "I am a bot ladpraod";
+{
+  "events": [
+      {
+        "replyToken": "pcAXV1P5Zz1B3ptcbkagTBMY1oaTTFYIQQWsW8jluVFnvJx3dImhgAzzmpmdgTJH+yayuggit1UJ9I6lKdYjyclHyLfE/YUe8/1fesCA6NbTVMgyigsmqSevfJ8+HP6ANpUSnoGXroRxYt/PtK8fdwdB04t89/1O/w1cDnyilFU=",
+        "type": "message",
+        "timestamp": 1462629479859,
+        "source": {
+             "type": "user",
+             "userId": "U2fdfed74441e474c1b103d0ff3ae35dd "
+         },
+         "message": {
+             "id": "325708",
+             "type": "text",
+             "text": "Hello, world"
+          }
+      }
+  ]
+}
