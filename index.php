5c44c63929e8782ca1056d1cdf3fb3886039bb58
arthers3king <31269678+arthers3king@users.noreply.github.com>
2017-08-23 14:13:28 +0700

    Update index.php

--- index.php
+++ index.php
@@ -1,18 +1,15 @@
-{
-  "events": [
-      {
-        "replyToken": "nHuyWiB7yP5Zw52FIkcQobQuGDXCTA",
-        "type": "message",
-        "timestamp": 1462629479859,
-        "source": {
-             "type": "user",
-             "userId": "U206d25c2ea6bd87c17655609a1c37cb8"
-         },
-         "message": {
-             "id": "325708",
-             "type": "text",
-             "text": "Hello, world"
-          }
-      }
-  ]
-}
+<?php
+$access_token = 'pcAXV1P5Zz1B3ptcbkagTBMY1oaTTFYIQQWsW8jluVFnvJx3dImhgAzzmpmdgTJH+yayuggit1UJ9I6lKdYjyclHyLfE/YUe8/1fesCA6NbTVMgyigsmqSevfJ8+HP6ANpUSnoGXroRxYt/PtK8fdwdB04t89/1O/w1cDnyilFU=';
+
+$url = 'https://api.line.me/v1/oauth/verify';
+
+$headers = array('Authorization: Bearer ' . $access_token);
+
+$ch = curl_init($url);
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
+$result = curl_exec($ch);
+curl_close($ch);
+
+echo $result;
